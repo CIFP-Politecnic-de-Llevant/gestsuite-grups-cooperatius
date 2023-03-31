654b180289b9940118fd199846b74754647abbf8
Joan Galmés Riera <joan.galmes@gmail.com>
2023-03-31 09:14:07 +0200

    Initial commit

---
+++ src/main/java/cat/iesmanacor/gestsuitegrupscooperatius/GestsuiteGrupsCooperatiusApplication.java
@@ -0,0 +1,13 @@
+package cat.iesmanacor.gestsuitegrupscooperatius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestsuiteGrupsCooperatiusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestsuiteGrupsCooperatiusApplication.class, args);
+    }
+
+}
