278d326e7c180d4c551647b70d4844e1e143508c
Joan Galmés Riera <joan.galmes@gmail.com>
2023-06-29 07:09:13 +0200

    Corregit bug amb l'objecte que es passava a la interfícies de comparació

--- src/main/java/cat/iesmanacor/gestsuitegrupscooperatius/dto/ItemGrupCooperatiuDto.java
+++ src/main/java/cat/iesmanacor/gestsuitegrupscooperatius/dto/ItemGrupCooperatiuDto.java
@@ -3,13 +3,13 @@ package cat.iesmanacor.gestsuitegrupscooperatius.dto;
 import cat.iesmanacor.gestsuitegrupscooperatius.model.ItemGrupCooperatiu;
 import lombok.Data;
 
-public @Data class ItemGrupCooperatiuDto implements Comparable<ItemGrupCooperatiu> {
+public @Data class ItemGrupCooperatiuDto implements Comparable<ItemGrupCooperatiuDto> {
     private ItemDto item;
     private GrupCooperatiuDto grupCooperatiu;
     private Integer percentatge;
 
     @Override
-    public int compareTo(ItemGrupCooperatiu o) {
+    public int compareTo(ItemGrupCooperatiuDto o) {
         return this.getItem().getIdItem().compareTo(o.getItem().getIdItem());
     }
 }
